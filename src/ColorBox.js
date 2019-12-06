a9c24bc0c7c86648bec5be66579f6dc2f6bc7dab
Liviu Preda <liviupreda@gmail.com>
2019-12-06 12:22:41 +0200

    display copy overlay on click; style copy overlay

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,11 +4,31 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./ColorBox.css";
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false
+    };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+
+  changeCopyState() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
+
   render() {
     const { name, background } = this.props;
+    const { copied } = this.state;
     return (
-      <CopyToClipboard text={background}>
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className="ColorBox">
+          <div
+            style={{ background }}
+            // Add show class to display overlay if copied is true
+            className={`copy-overlay ${copied && "show"}`}
+          />
           <div className="copy-container">
             <div className="box-content">
               <span>{name}</span>
