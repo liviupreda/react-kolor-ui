4a6e1c78e204e892f69113ba77a48c639cd306d4
Liviu Preda <liviupreda@gmail.com>
2019-12-07 16:14:21 +0200

    import colorHelpers to App.js; finish generating color shades in colorHelpers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Palette from "./Palette";
 import seedPalettes from "./seedPalettes";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(seedPalettes[6]));
   return <Palette {...seedPalettes[6]} />;
 }
 
--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -1,30 +1,3 @@
-// {
-//   paletteName: "Material UI Colors",
-//   id: "material-ui-colors",
-//   emoji: "🎨",
-//   colors: [
-//     { name: "red", color: "#F44336" },
-//     { name: "pink", color: "#E91E63" },
-//     { name: "purple", color: "#9C27B0" },
-//     { name: "deeppurple", color: "#673AB7" },
-//     { name: "indigo", color: "#3F51B5" },
-//     { name: "blue", color: "#2196F3" },
-//     { name: "lightblue", color: "#03A9F4" },
-//     { name: "cyan", color: "#00BCD4" },
-//     { name: "teal", color: "#009688" },
-//     { name: "green", color: "#4CAF50" },
-//     { name: "lightgreen", color: "#8BC34A" },
-//     { name: "lime", color: "#CDDC39" },
-//     { name: "yellow", color: "#FFEB3B" },
-//     { name: "amber", color: "#FFC107" },
-//     { name: "orange", color: "#FF9800" },
-//     { name: "deeporange", color: "#FF5722" },
-//     { name: "brown", color: "#795548" },
-//     { name: "grey", color: "#9E9E9E" },
-//     { name: "bluegrey", color: "#607D8B" }
-//   ]
-// }
-
 import chroma from "chroma-js";
 const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
@@ -39,15 +12,39 @@ function generatePalette(starterPalette) {
     newPalette.colors[level] = [];
   }
   for (let color of starterPalette.colors) {
+    let scale = generateScale(color.color, 10).reverse();
+    for (let i in scale) {
+      newPalette.colors[levels[i]].push({
+        name: `${color.name} ${levels[i]}`,
+        // Replace any spaces in color name with a dash; will use in routes
+        id: color.name.toLowerCase().replace(/ /g, "-"),
+        hex: scale[i],
+        // Use Chroma to generate RGB color from Hex value
+        rgb: chroma(scale[i]).css(),
+        rgba: chroma(scale[i])
+          .css()
+          .replace("rgb", "rgba")
+          .replace(")", ",1.0)")
+      });
+    }
   }
+  return newPalette;
 }
 
 function generateRange(hexColor) {
-  // Generate an array of shades for hexColor: [hexColor.darken(1.4) - hexColor - white(#fff)]
+  // Generate an array of ranges for hexColor shade scale:
+  // [hexColor.darken(1.4), hexColor, #fff]
   const end = "#fff";
   return [chroma(hexColor).darken(1.4), hexColor, end];
 }
 
 function generateScale(hexColor, numberOfColors) {
-  chroma.scale();
+  // Generate scale from the range generated in generateRange()
+  // Spits out a number of *numberofColors* colors
+  return chroma
+    .scale(generateRange(hexColor))
+    .mode("lab")
+    .colors(numberOfColors);
 }
+
+export { generatePalette };
