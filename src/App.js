6204a5b8a30d80880a7f9021d633c6a420f47654
Liviu Preda <liviupreda@gmail.com>
2019-12-09 13:54:48 +0200

    add react router & basic routes in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import Palette from "./Palette";
 import seedPalettes from "./seedPalettes";
 import { generatePalette } from "./colorHelpers";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Palette palette={generatePalette(seedPalettes[6])} />;
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette List</h1>} />
+        <Route exact path="/palette/:id" render={() => <h1>Palette ID</h1>} />
+      </Switch>
+      {/* <div>
+        <Palette palette={generatePalette(seedPalettes[6])} />;
+      </div> */}
+    </Router>
   );
 }
 
