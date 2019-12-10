91a18be7ca138bd4b387df45d253488937d9dc50
Liviu Preda <liviupreda@gmail.com>
2019-12-10 12:49:06 +0200

    implement findPalette in App.js and fix Palette Route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import Palette from "./Palette";
 import seedPalettes from "./seedPalettes";
 import { generatePalette } from "./colorHelpers";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" render={() => <h1>Palette List</h1>} />
-        <Route exact path="/palette/:id" render={() => <h1>Palette ID</h1>} />
-      </Switch>
-      {/* <div>
-        <Palette palette={generatePalette(seedPalettes[6])} />;
-      </div> */}
-    </Router>
-  );
+class App extends Component {
+  // Parse the Palette array in seedPalettes.js and return the pallete with id === x
+  findPalette(id) {
+    return seedPalettes.find(palette => palette.id === id);
+  }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Palette List</h1>} />
+          <Route
+            exact
+            path="/palette/:id"
+            render={routeProps => (
+              <Palette
+                palette={generatePalette(
+                  this.findPalette(routeProps.match.params.id)
+                )}
+              />
+            )}
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
