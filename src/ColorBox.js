b5e4c6d94c53280578b8d78a288776c0f56b61ec
Liviu Preda <liviupreda@gmail.com>
2019-12-14 08:25:56 +0200

    implement dynamic text color based on luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-
+import chroma from "chroma-js";
 import "./ColorBox.css";
 
 class ColorBox extends Component {
@@ -22,6 +22,9 @@ class ColorBox extends Component {
   render() {
     const { name, background, paletteId, colorId, showLink } = this.props;
     const { copied } = this.state;
+    // Use chroma to dinamically change text brightness, based on ColorBox background color
+    const isDarkColor = chroma(background).luminance() <= 0.06;
+    const isLightColor = chroma(background).luminance() >= 0.6;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className="ColorBox">
@@ -32,20 +35,24 @@ class ColorBox extends Component {
           />
           <div className={`copy-msg ${copied && "show"}`}>
             <h1>Copied!</h1>
-            <p>{background}</p>
+            <p className={isLightColor && "dark-text"}>{background}</p>
           </div>
           <div className="copy-container">
             <div className="box-content">
-              <span>{name}</span>
+              <span className={isDarkColor && "light-text"}>{name}</span>
             </div>
-            <button className="copy-button">Copy</button>
+            <button className={`copy-button ${isLightColor && "dark-text"}`}>
+              Copy
+            </button>
           </div>
           {showLink && (
             <Link
               to={`/palette/${paletteId}/${colorId}`}
               onClick={e => e.stopPropagation()}
             >
-              <span className="btn-more">More</span>
+              <span className={`btn-more ${isLightColor && "dark-text"}`}>
+                More
+              </span>
             </Link>
           )}
           {/* stopPropagation() stops event bubbling to parent elements.
