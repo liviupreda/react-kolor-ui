5f4e4b8a000698ff21ec2f29bfe900f0e487796d
Liviu Preda <liviupreda@gmail.com>
2019-12-16 10:17:20 +0200

    continue moving jss styles to separate folder & files

--- src/PaletteFooter.js
+++ src/PaletteFooter.js
@@ -1,20 +1,6 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-
-const styles = {
-  footer: {
-    backgroundColor: "white",
-    height: "5vh",
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    fontWeight: "bold"
-  },
-  footerEmoji: {
-    fontSize: "1.5rem",
-    margin: "1rem"
-  }
-};
+import styles from "./styles/PaletteFooterStyles";
 
 function PaletteFooter(props) {
   const { paletteName, emoji, classes } = props;
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,41 +1,9 @@
 import React, { Component } from "react";
 import MiniPalette from "./MiniPalette";
-// import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
-
-const styles = {
-  root: {
-    backgroundColor: "cornflowerblue",
-    height: "100vh",
-    display: "flex",
-    alignItems: "flex-start",
-    justifyContent: "center"
-  },
-  container: {
-    width: "50%",
-    display: "flex",
-    alignItems: "flex-start",
-    flexDirection: "column",
-    flexWrap: "wrap"
-  },
-  nav: {
-    display: "flex",
-    width: "100%",
-    justifyContent: "space-between",
-    color: "white"
-  },
-  paletteList: {
-    boxSizing: "border-box",
-    width: "100%",
-    display: "grid",
-    gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%"
-  }
-};
+import styles from "./styles/PaletteListStyles";
 
 class PaletteList extends Component {
-  // Method routing to individual Palette by clicking on MiniPalette
-  // We use props.history instead of Link bc of best practices and styling reasons
   goToPalette(id) {
     this.props.history.push(`/palette/${id}`);
   }
