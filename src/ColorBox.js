0ac676596c980f50c3ae9c512207b146debed7de
Liviu Preda <liviupreda@gmail.com>
2019-12-16 08:53:56 +0200

    finish refactoring ColorBox JSS

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -79,6 +79,57 @@ const styles = {
     letterSpacing: "1px",
     textTransform: "uppercase",
     fontSize: "12px"
+  },
+  copyOverlay: {
+    opacity: "0",
+    zIndex: "0",
+    width: "100%",
+    height: "100%",
+    transition: "transform 0.6s ease-in-out",
+    transform: "scale(0.1)"
+  },
+  showCopyOverlay: {
+    opacity: "1",
+    transform: "scale(50)",
+    zIndex: "10",
+    position: "absolute"
+  },
+  copyMessage: {
+    position: "fixed",
+    left: "0",
+    right: "0",
+    top: "0",
+    bottom: "0",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    fontSize: "4rem",
+    transform: "scale(0.1)",
+    opacity: "0",
+    color: "white",
+    "& h1": {
+      fontWeight: "400",
+      textShadow: "1px 2px black",
+      background: "rgba(255, 255, 255, 0.2)",
+      width: "100%",
+      textAlign: "center",
+      marginBottom: "0",
+      padding: "1rem",
+      textTransform: "uppercase"
+    },
+    "& p": {
+      fontSize: "2rem",
+      fontWeight: "200",
+      textTransform: "uppercase"
+    }
+  },
+  showCopyMessage: {
+    opacity: "1",
+    transform: "scale(1)",
+    zIndex: "25",
+    transition: "all 0.3s ease-in-out",
+    transitionDelay: "0.3s"
   }
 };
 
@@ -112,9 +163,13 @@ class ColorBox extends Component {
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`copy-overlay ${copied && "show"}`}
+            className={`${classes.copyOverlay} ${copied &&
+              classes.showCopyOverlay}`}
           />
-          <div className={`copy-msg ${copied && "show"}`}>
+          <div
+            className={`${classes.copyMessage} ${copied &&
+              classes.showCopyMessage}`}
+          >
             <h1>Copied!</h1>
             <p className={classes.copyText}>{background}</p>
           </div>
