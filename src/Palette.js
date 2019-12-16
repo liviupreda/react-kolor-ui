68fd6e5ecb18b26a486c2395aa316790ff0d6627
Liviu Preda <liviupreda@gmail.com>
2019-12-16 10:04:12 +0200

    move ColorBox JSS in separate file; refactor PaletteFooter styling into JSS

--- src/Palette.js
+++ src/Palette.js
@@ -13,18 +13,6 @@ const styles = {
   },
   colors: {
     height: "90%"
-  },
-  footer: {
-    backgroundColor: "white",
-    height: "5vh",
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    fontWeight: "bold"
-  },
-  footerEmoji: {
-    fontSize: "1.5rem",
-    margin: "1rem"
   }
 };
 
--- src/PaletteFooter.js
+++ src/PaletteFooter.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+  footer: {
+    backgroundColor: "white",
+    height: "5vh",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    fontWeight: "bold"
+  },
+  footerEmoji: {
+    fontSize: "1.5rem",
+    margin: "1rem"
+  }
+};
 
 function PaletteFooter(props) {
-  const { paletteName, emoji } = props;
+  const { paletteName, emoji, classes } = props;
   return (
-    <footer className="Palette-footer">
+    <footer className={classes.footer}>
       {paletteName}
-      <span className="Palette-footer-emoji">{emoji}</span>
+      <span className={classes.footerEmoji}>{emoji}</span>
     </footer>
   );
 }
 
-export default PaletteFooter;
+export default withStyles(styles)(PaletteFooter);
